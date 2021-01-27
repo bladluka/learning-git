645d18d06dd9761e82c89a90c7bf1a0a4c17a28a
bladluka <78096877+bladluka@users.noreply.github.com>
2021-01-27 21:00:17 +0100

    druga edycja

--- main.py
+++ main.py
@@ -4,3 +4,4 @@ for number in lista_zakupow:
     print(f"Idę do {number.capitalize()} kupuję tu nastepujące rzeczy {[number2.capitalize() for number2 in lista_zakupow.get(number)]}")
 print(f"W sumie kupuję {sum(len(item) for item in lista_zakupow.values())} produktów")
 print('pierwsza edycja')
+print('druga edycja')

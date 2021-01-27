0a5cbda3304f3708397703a052596f38a89074c9
bladluka <78096877+bladluka@users.noreply.github.com>
2021-01-27 20:57:14 +0100

    pierwsza edycja

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ lista_zakupow = {'piekarnia': ['chleb', 'pączek', 'bułki'], 'warzywniak': ['ma
 for number in lista_zakupow:
     print(f"Idę do {number.capitalize()} kupuję tu nastepujące rzeczy {[number2.capitalize() for number2 in lista_zakupow.get(number)]}")
 print(f"W sumie kupuję {sum(len(item) for item in lista_zakupow.values())} produktów")
+print('pierwsza edycja')
